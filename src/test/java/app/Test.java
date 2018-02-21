6345a36b21823850de6ca44698c64f71d3ef142c
NOTB724\PoliL <luca.poli@scai-consulting.grupposcai.it>
2018-02-21 16:08:44 +0100

    TEST API SERACHCITY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/test/java/app/Test.java
+++ src/test/java/app/Test.java
@@ -2,11 +2,29 @@ package app;
 
 import static org.junit.Assert.*;
 
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class Test {
+    @Autowired
+	private MockMvc mockMvc;
 
+	
 	@org.junit.Test
-	public void test() {
-		assert(true);
+	public void test() throws Exception {
+		mockMvc.perform(get("/searchCity").param("search", "bologna"))
+				.andExpect(content().string(containsString("Bologna")));
 	}
-
 }
