64597d5afd0a38377fcf0ca8af3eeb98b799befa
cristiciobanu <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-02-26 11:58:21 +0100

    lang.js refactor

--- src/main/resources/static/js/lang.js
+++ src/main/resources/static/js/lang.js
@@ -24,15 +24,10 @@ var lang = (function () {
   };
 
   var stampa = function(data) {
-
 		for(var i = 0; i < Object.keys(data).length; i++){
-			if(data[i].iso == "en"){
-					$select.append("<option selected='selected' value="+data[i].iso+">"+data[i].name+"</option>");
-			}
-			else {
-				$select.append("<option value="+data[i].iso+">"+data[i].name+"</option>");
-			}
+			$select.append("<option value="+data[i].iso+">"+data[i].name+"</option>");
 		}
+		$("select option[value='en']").attr("selected","selected");
   };
   /* END PRIVATE BUSINESS FUNCTIONS */
 
